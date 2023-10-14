100996410545e54aec0648ebde99ecc79fa7e5bb
kbbmsmpskvas <kmyaccb@gmail.com>
2023-10-14 23:41:15 +0530

    my first commit

---
+++ db.py
@@ -0,0 +1,190 @@
+from dataclasses import dataclass
+from pickletools import read_uint1
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+from datetime import datetime
+import pytz
+import hashlib
+
+scope = ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/auth/spreadsheets',
+            "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
+creds = ServiceAccountCredentials.from_json_keyfile_name("dsa.json", scope)
+opener = gspread.authorize(creds)
+sheet = opener.open("DSA-BOOTCAMP")
+
+def rows(work):
+    wks = sheet.worksheet(work)
+    return len(wks.get_all_records())
+
+def allRec(work):
+    wks = sheet.worksheet(work)
+    return wks.get_all_records()
+
+def addUser(fname, name, email, pswd):
+    wks = sheet.worksheet("users")
+    n = rows("users")
+    n += 2
+
+    UTC = pytz.utc
+    timeZ_Kl = pytz.timezone('Asia/Kolkata')
+    dt_Kl = datetime.now(timeZ_Kl)
+    utc_Kl = dt_Kl.astimezone(UTC)
+    dt = dt_Kl.strftime('%Y-%m-%d %H:%M:%S')
+    
+    wks.update_cell(n, 1, name)
+    wks.update_cell(n, 2, fname)
+    wks.update_cell(n, 3, email)
+    wks.update_cell(n, 4, pswd)
+    wks.update_cell(n, 5, str(dt))
+
+def check_username(username):
+    wks = sheet.worksheet("users")
+    data = wks.get_all_records()
+
+    for i in data:
+        if i['username'] == username:
+            return str(i['password'])
+    return False
+
+def check_mail(mail):
+    wks = sheet.worksheet("users")
+    data = wks.get_all_records()
+
+    for i in data:
+        if i['email'] == mail:
+            return True
+    return False
+
+    print(data)
+
+def enroll(course, usnm):
+    wks = sheet.worksheet("enrolled")
+    n = rows("enrolled")
+    data = list(wks.get_all_values())
+    data.pop(0)
+    allEnrolled = []
+    for i in data:
+        allEnrolled.append(i[int(course) - 1])
+
+    if allEnrolled.count(usnm) == 0:
+        count = 1
+        for i in data:
+            if i[int(course) - 1] != '':
+                count += 1
+
+        wks.update_cell(count + 1, course, usnm)
+
+def getEnCourses(username):
+    wks = sheet.worksheet("enrolled")
+    data = list(wks.get_all_values())
+    all = []
+
+    for i in data:
+        if username in i:
+            col = 0
+            for j in i:
+                col += 1
+                if j == username:
+                    all.append(col)
+    return all
+
+def getDetails(course):
+    wks = sheet.worksheet("courses")
+    data = list(wks.get_all_values())
+
+    details = []
+    for i in data:
+        if i[0] == str(course):
+            details.append(i[1])
+            details.append(i[2])
+            
+    return details
+
+def getLectures(course):
+    wks = sheet.worksheet(course)
+    data = list(wks.get_all_values())
+    data.pop(0)
+
+    links = []
+    for i in data:
+        lst = []
+        lst.append(i[0])
+        lst.append(i[1])
+        lst.append(i[2])
+        lst.append(i[3])
+        # lst : [lecture number, lecture link, thumbnail link, attachment link]
+
+        links.append(lst)
+        # links: [[ith lecture number, ith lecture link, ith thumbnail link, ith attachment link], ...]
+
+    return links
+
+def getName(username):
+    wks = sheet.worksheet("users")
+    data = wks.get_all_records()
+
+    for i in data:
+        if i['username'] == username:
+            name = ""
+            for j in str(i["full name"]):
+                if j == " ":
+                    break
+                else:
+                    name += j
+            return name
+
+# function to record number of visits in a page by any user
+def traffic(username, id):
+    wks = sheet.worksheet("traffic")
+    data = wks.get_all_records()
+    
+    row = 0
+    value = 0
+
+    for i in range(len(data)):
+        if data[i]['username'] == username:
+            row = i + 1
+            value = data[i][id]
+            break
+
+
+    if row == 0:
+        wks.update_cell(len(data) + 2, 2, 1)
+        wks.update_cell(len(data) + 2, 1, username)
+    else:
+        wks.update_cell(row + 1, 2, value + 1)
+
+def getQuiz():
+    wks = sheet.worksheet("Quiz")
+    data = wks.get_all_records()
+
+    name = []
+    link = []
+
+    for i in data:
+        name.append(i['name'])
+        link.append(i['link'])
+
+    final = zip(name, link)
+    # print(name)
+    return final
+
+
+def change(mail, password):
+    wks = sheet.worksheet("users")
+    data = list(wks.get_all_values())
+    
+    n = len(data)
+    row = -1;
+    for i in range(0, n):
+        if data[i][2] == mail:
+            row = i;
+            break
+
+    if row == -1:
+        return -1;
+    else:
+        password = hashlib.md5(password.encode()).hexdigest()
+        wks.update_cell(row + 1, 4, password)
+        s = "0" + str(data[row][0])
+        return s
---
+++ main/views.py
@@ -0,0 +1,276 @@
+from email.mime import image
+from http.client import HTTPResponse
+from django.http import HttpResponse 
+from multiprocessing import context
+import re
+from django.shortcuts import render, redirect
+from flask import request
+import db
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+from django.contrib import messages
+import hashlib
+import json
+from rest_framework.decorators import api_view
+
+def getContext(cookie):
+    names = cookie['names']
+    number = cookie['number']
+    images = cookie['images']
+    fname = cookie['fname']
+
+    names = names[1:]
+    names = names.rstrip(names[-1])
+    names = names.split(', ')
+
+    number = number[1:]
+    number = number.rstrip(number[-1])
+    number = number.split(', ')
+
+    images = images[1:]
+    images = images.rstrip(images[-1])
+    images = images.split(', ')
+
+    for i in range(0, len(names)):
+        names[i] = names[i][1:]
+        names[i] = names[i].rstrip(names[i][-1])
+
+    for i in range(0, len(number)):
+        number[i] = number[i][1:]
+        number[i] = number[i].rstrip(number[i][-1])
+
+    for i in range(0, len(images)):
+        images[i] = images[i][1:]
+        images[i] = images[i].rstrip(images[i][-1])
+
+    context = {"data": zip(number, names, images), "fname": fname}
+    return context
+
+def home(request):
+    cookie = request.COOKIES
+    try:
+        if cookie["login"] == "1":
+            context = getContext(cookie)
+            return render(request, "afterLog.html", context)
+        else:
+            print("redirected")
+            return render(request, "index.html")
+    except:
+        return render(request, "index.html")
+
+
+def edit(request):
+    x = db.rows("users")
+    db.addUser("piyush", "xyz", "abcd")
+    return render(request, "index.html")
+
+def checkSpace(name):
+    for i in name:
+        if i == ' ':
+            return True
+    return False
+
+def signup(request):
+    d = request.POST
+    print(d)
+    name = d['fname'] + " " + d["lname"]
+    print(name)
+    usnm = d['usnm']
+    mail = d['email']
+    passd = d['passd']
+    repass = d['re-passd']
+
+    space = checkSpace(usnm)
+
+    if (passd != repass):
+        # unmatched password
+        messages.info(request, 2)
+        return render(request, "signup.html")
+
+    if (space):
+        messages.info(request, 7)
+        return render(request, "signup.html")
+
+    check1 = db.check_username(usnm)
+    check2 = db.check_mail(mail)
+
+    if check1:
+        # username exists
+        messages.info(request, 1)
+        return render(request, "signup.html")
+    
+    if check2:
+        # email exists
+        messages.info(request, 3)
+        return render(request, "signup.html")
+
+    hashed = hashlib.md5(passd.encode()).hexdigest()
+    messages.info(request, 8)
+    db.addUser(name, usnm, mail, hashed)
+    return redirect("/log")
+
+def login(request):
+    cookie = request.COOKIES
+    # if logged in already, don't read all the data again from db
+    try:
+        if (cookie['login'] == '1'):
+            context = getContext(cookie)
+            return render(request, "afterLog.html", context)
+    except:
+        data = request.POST
+        usnm = data['usnm']
+        passd = data['passd']
+
+        hashed = hashlib.md5(passd.encode()).hexdigest()
+        check = db.check_username(usnm)
+
+        if check:
+            if check == hashed:
+                # logged in
+                # messages.info(request, 4)
+                enrolled = db.getEnCourses(usnm)
+                name = []
+                image = []
+                number = []
+
+                for i in enrolled:
+                    details = db.getDetails(i)
+                    number.append(str(i))
+                    name.append(details[0])
+                    image.append(details[1])
+
+                fname = db.getName(usnm)
+                final = zip(number, name, image)
+                context = {"data": final, "fname": fname}
+                response = render(request, "afterLog.html", context)
+
+                # cookies
+                response.set_cookie('login', '1')
+                response.set_cookie("username", usnm)
+                response.set_cookie("names", name)
+                response.set_cookie("images", image)
+                response.set_cookie("number", number)
+                response.set_cookie("fname", fname)
+
+                return response
+            else:
+                # wrong password
+                messages.info(request, 5)
+                return render(request, "login.html")
+        else:
+            # no account
+            messages.info(request, 6)
+            return render(request, "login.html")
+
+        return render(request, "index.html")
+
+def enroll(request, course_id):
+    cook = request.COOKIES
+    data = request.POST
+    course = str(course_id)
+    usnm = cook["username"]
+
+    pwd = db.check_username(usnm)
+    if pwd:
+        db.enroll(course, usnm)
+        enrolled = db.getEnCourses(usnm)
+        name = []
+        image = []
+        number = []
+
+        for i in enrolled:
+            details = db.getDetails(i)
+            number.append(str(i))
+            name.append(details[0])
+            image.append(details[1])
+
+        final = zip(number, name, image)
+        context = {"data": final}
+        response = redirect("/")
+
+        # cookies
+        response.set_cookie("names", name)
+        response.set_cookie("images", image)
+        response.set_cookie("number", number)
+
+        return response
+
+    # response = redirect('/')
+    return login(request)
+
+def course(request, course_id):
+    cookie = request.COOKIES
+    # db.traffic(str(cookie["username"]), course_id)
+    try:
+        if (cookie["login"] == "1"):
+            course = "course" + str(course_id)
+            lecs = db.getLectures(course)
+            name = db.getDetails(course_id)[0]
+
+            enrolled = []
+            data = getContext(cookie)
+            for i, j, k in data["data"]:
+                enrolled.append(i)
+
+            if str(course_id) in enrolled:
+                context = {"data": lecs, "name": name}
+                db.traffic(str(cookie["username"]), str(course_id))
+
+                return render(request, "lectures.html", context)
+            else:
+                return redirect("/")
+        else:
+            return redirect("/")
+    except:
+        return redirect("/")
+
+    
+
+def logout(request):
+    cookie = request.COOKIES
+    response = render(request, "index.html")
+    try:
+        if (cookie["login"] == "1"):
+            response.delete_cookie("login", "1")
+            return response
+        else:
+            return redirect("/")
+    except:
+        return redirect("/")
+
+# def myCourses(request):
+#     context = getContext(request.COOKIES)
+#     return render(request, "afterLog.html", context)
+
+
+def teams(request):
+    return render(request, "aboutus.html")
+
+def log(request):
+    return render(request, "login.html")
+
+def reg(request):
+    return render(request, "signup.html")
+
+def quiz(request):
+    cook = request.COOKIES
+    quiz = db.getQuiz()
+    context = {"fname": cook['fname'], "data": quiz}
+    return render(request, "quiz.html", context)
+
+def update(request):
+    return render(request, "update.html")
+
+def change(request):
+    data = request.POST
+    email = data['email']
+    passd = data['passd']
+    
+    get = db.change(email, passd)
+
+    if get == -1:
+        messages.info(request, get)
+        return render(request, "update.html")
+    else:
+        messages.info(request, get)
+        return render(request, "login.html")
